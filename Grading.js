9e7e66f39e928801c1c65c3bdcbe6fca2076b826
Priyadarshan <skullboydanger1@gmail.com>
2020-07-08 11:05:01 +0530

    Grading

---
+++ Grading.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    let finalGrade = [];
+    for (let grade of grades){
+        if(grade > 35){
+            let diff = Math.ceil(grade/5)*5 - grade;
+            if(diff < 3){
+                finalGrade.push(Math.ceil(grade/5)*5);
+            }
+            else{
+                finalGrade.push(grade);
+            }
+        }
+        else{
+            finalGrade.push(grade);
+        }
+    }
+    return (finalGrade);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
